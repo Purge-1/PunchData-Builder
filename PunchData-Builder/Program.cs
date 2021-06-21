6b09549f158f328aff0503092c49a020159759fc
Purge-1 <80547719+Purge-1@users.noreply.github.com>
2021-06-21 05:57:47 -0700

    Add files via upload

---
+++ PunchData-Builder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace PunchData_Builder
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Growtopia - PunchData Builder";
+            Console.Clear();
+            Console.WriteLine("_____________________________________________________________");
+            Console.WriteLine("");
+            Console.WriteLine("                          Growtopia");
+            Console.WriteLine("                       PunchData Builder");
+            Console.WriteLine("                      By purge_dev");
+            Console.WriteLine($"__________________________{DateTime.Now.ToString("d/MM/yyyy/h/m")}__________________________");
+            PunchData data = new PunchData();
+            data.AddEffectToList();
+            data.Build();
+            Console.WriteLine("Press any key to close..."); 
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
